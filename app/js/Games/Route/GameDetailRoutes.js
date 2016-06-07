8d6747bc6bab9ee24017b344807cd10eaf98922a
pmulders1 <pmulders1@student.avans.nl>
2016-06-07 14:14:00 +0200

    Tabs in gamedetail

--- app/js/Games/Route/GameDetailRoutes.js
+++ app/js/Games/Route/GameDetailRoutes.js
@@ -1,11 +1,22 @@
-module.exports = function($stateProvider) {
-
+module.exports = function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.when('/gamedetail/:id', '/gamedetail/:id/details');
     $stateProvider
-
         .state('gamedetail', {
         	url: '/gamedetail/:id',
             params: {'id': null },
-            templateUrl : './js/Games/Views/GameDetailView.html',
+            templateUrl : './js/Games/Views/GameSingleView.html',
             controller  : 'GameDetailController as gDetailController'
+        })
+        .state('gamedetail.details', {
+        	url: '/details',
+            templateUrl : './js/Games/Views/GameDetailView.html'
+        })
+        .state('gamedetail.board', {
+        	url: '/board',
+            templateUrl : './js/Games/Views/GameBoardView.html'
+        })
+        .state('gamedetail.players', {
+        	url: '/players',
+            templateUrl : './js/Games/Views/GamePlayerView.html'
         })
 };
--- app/js/Home/Controllers/HomeController.js
+++ app/js/Home/Controllers/HomeController.js
@@ -1,6 +1,6 @@
 module.exports = function($scope, $rootScope){
 	var self = this;
 	$scope.themes = ['indie', 'titillium', 'montserrat', 'josefin', 'architects'];
-	$scope.currentTheme = "indie";
+	$scope.currentTheme = "titillium";
 	$scope.message = "Welkom op de Majong website van Paul Mulders en Quinn van Haastrecht";
 }
